55d7cd919a6eceaa52dbecf5ef8d38078801fc44
noble6 <bitcoineazy@gmail.com>
2021-11-08 06:41:43 +0300

    Recipe's urls

--- backend/foodgram/recipes/urls.py
+++ backend/foodgram/recipes/urls.py
@@ -5,7 +5,12 @@ from .views import *
 
 router = DefaultRouter()
 router.register('tags', TagViewSet, basename='tags')
+router.register('ingredients', IngredientViewSet, basename='ingredients')
+router.register('recipes', RecipesViewSet, basename='recipes')
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('recipes/<int:recipe_id>/shopping_cart/', CartView.as_view()),
+    path('recipes/download_shopping_cart/', download_cart,
+         name='download_shopping_cart'),
 ]
