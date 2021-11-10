15f31ef177a1d8bfb88c77be4a6a41ca65588d30
noble6 <bitcoineazy@gmail.com>
2021-11-10 21:42:39 +0300

    update recipe fix

--- backend/foodgram/recipes/serializers.py
+++ backend/foodgram/recipes/serializers.py
@@ -104,15 +104,20 @@ class RecipeSerializer(serializers.ModelSerializer):
         return recipe
 
     def update(self, recipe, validated_data):
-        ingredients_in_recipe = validated_data.pop('ingredients')
+        ingredients = validated_data.pop('ingredients')
         tags_data = validated_data.pop('tags')
+        recipe.ingredients.clear()
+        ingredient_in_recipe = [IngredientForRecipe(
+            recipe=recipe,
+            ingredient_id=ingredient['id'],
+            amount=ingredient['amount']) for ingredient in ingredients]
+        IngredientForRecipe.objects.bulk_create(ingredient_in_recipe)
         if validated_data.get('image') is not None:
-            recipe.image = validated_data.get('image', recipe.image)
+            recipe.image = validated_data.get('image')
         recipe.name = validated_data.pop('name')
         recipe.text = validated_data.pop('text')
         recipe.cooking_time = validated_data.pop('cooking_time')
         recipe.save()
-        recipe.ingredients.set(ingredients_in_recipe)
         recipe.tags.set(tags_data)
         return recipe
 
